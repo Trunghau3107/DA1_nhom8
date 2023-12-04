fe62ccb5425225b0bf90ee0d1ddb17a72cad734a
yen <yenhuynhhai12082004@gmail.com>
2023-12-04 08:03:23 +0700

    yen

--- models/pdo.php
+++ models/pdo.php
@@ -7,7 +7,7 @@ function pdo_get_connection()
 {
     $dburl = "mysql:host=localhost;dbname=jewelry;charset=utf8";
     $username = 'root';
-    $password = ';suhefn;';
+    $password = '';
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
